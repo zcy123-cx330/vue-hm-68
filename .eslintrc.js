eafa8cc2817bad3824812cb8a53ccee218568d77
1042825973@qq.com <1042825973@qq.com>
2020-08-28 15:24:53 +0800

    项目配置了路由

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     // key: 规则的名字   off: 关闭这条规则
-    'space-before-function-paren': ['error', 'never']
+    // 'space-before-function-paren': ['error', 'never']
+    'space-before-function-paren': ['off']
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
 Vue.use(VueRouter)
 
 const routes = [
-
+  { path: '/login', component: Login },
+  { path: '/Register', component: Register }
 ]
 
 const router = new VueRouter({
